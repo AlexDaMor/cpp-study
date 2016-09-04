9b9a112a7f8bc5b7a393ad835d8973b780faa840
AlexDaMor <karasev.alexander@gmail.com>
2016-09-04 20:41:53 +1000

    Headers commit

--- hdr/sort.hpp
+++ hdr/sort.hpp
@@ -1,13 +1,14 @@
 
 
-int* bubble_sort    (int* source);
-int* insertion_sort (int* source);
-int* merge_sort     (int* source);
-int* quick_sort     (int* source);
-int* heap_sort      (int* source);
-int* selection_sort (int* source);
+int* bubble_sort    (int* source,int size);
+int* insertion_sort (int* source,int size);
+int* merge_sort     (int* source,int size);
+int* quick_sort     (int* source,int size);
+int* heap_sort      (int* source,int size);
+int* selection_sort (int* source,int size);
 
 
 int* create_source  (int size, int range); 
 //returns ptr to an array with [size] random elemets with values from 0 to [range]
 void show_array(int* source, int size, int space, int row_length);
+//prints source array of size elements, col width=space, col quantity=row_length
